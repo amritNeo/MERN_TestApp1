68b87cdaa98dea0b793b4bde4bfbf0e33ca95c7a
amrit mishra <amritmishra123456789@gmail.com>
2024-06-01 12:11:17 +0530

    CORS fix

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,15 @@ const router =  require('./routes')
 
 const app =  express()
 app.use(cookieParser())
+// app.use(cors({
+//     origin : process.env.FRONTEND_URL,
+//     credentials: true
+// }));
+
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials: true
+    origin :'https://mern-test-app1.vercel.app',
 }));
+
 app.use(express.json())
 app.use("/api",router)
 
